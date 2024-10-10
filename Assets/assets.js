90bdbb811c2169cec222296514954873630bfff8
Ojee Afolabi <sylvesterojay@gmail.com>
2024-10-10 12:54:07 +0100

    navbar, heading and subscribe option

--- Assets/assets.js
+++ Assets/assets.js
@@ -1,3 +1,12 @@
-export const assets ={
-    
+import logo from "./logo.svg";
+import rightarrow from "./right-arrow.svg"
+import email from "./email.svg"
+
+
+
+
+export const assets = {
+    logo,
+    rightarrow,
+    email
 }
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,10 +1,48 @@
+import { assets } from "@/Assets/assets";
+import Image from "next/image";
 import React from "react";
+import { CiMail } from "react-icons/ci";
 
 const NavBar = () => {
   return (
     <div className="py-5 px-5 md:px-12 lg:px-28">
       <div className="flex justify-between items-center">
-        
+        <Image
+          src={assets.logo}
+          width={40}
+          alt=""
+          className="w-[30px] md:w-[50px] lg:w-[60px]"
+        />
+        <button className="flex items-center rounded shadow-[-7px_7px_0px_#000000] gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black">
+          Get started{" "}
+          <Image
+            src={assets.rightarrow}
+            alt="right-arrow"
+            className="w-[30px]"
+          />
+        </button>
+      </div>
+      <div className="text-center my-8">
+        <h1 className="font-medium text-3xl sm:text-5xl">Latest Blogs</h1>
+        <p className="mt-10 max-w-[740px] m-auto text-xs sm:text-base">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, nisi?
+        </p>
+        <form
+          className="flex border rounded border-solid border-black justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 shadow-[-7px_7px_0px_#000000] "
+          action=""
+        >
+          <input
+            className="pl-4 outline-none"
+            type="email"
+            placeholder="enter your email"
+          />
+          <button
+            type="submit"
+            className="flex gap-2 items-center border-l border-black py-4 px-4 sm:px-8 active:bg-gray-600 active:text-white"
+          >
+            Subscribe <CiMail size={30} />
+          </button>
+        </form>
       </div>
     </div>
   );
